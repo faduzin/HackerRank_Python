a90ea36f410ecba2b0ab0686e798152d77c38912
faduzin <eric.fadul@gmail.com>
2025-01-11 23:35:37 -0300

    SetAdd

---
+++ SetAdd.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__' :
+    n = int(input())
+    st = set()
+    for _ in range(n):
+        st.add(input())
+    print(len(st))
+
+# What I learned:
+# 1. How to add values into a set
