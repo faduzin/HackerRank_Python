1838139b7f4de516b87fcfff765cdc160bd7ea39
faduzin <eric.fadul@gmail.com>
2025-01-25 23:19:37 -0300

    ReStartEnd - Skipped

--- ReStartEnd.py
+++ ReStartEnd.py
@@ -4,10 +4,15 @@ if __name__ == '__main__' :
     s = input().strip()
     k = input().strip()
     l = []
-    for i in range (len(k)+1):
+    i = 0
+    while i <= (len(k)):
         match = re.search(k, s[i:])
         if match:
-            l.append(tuple([match.start() + i, match.end() - 1 + i])) 
+            l.append(tuple([match.start() + i, match.end() - 1 + i]))
+            i += int(match.span()[0]) + 1 
+        else: 
+            break
+    if len(l) == 0:
+        l.append((-1, -1))
     for item in l:
         print(item)
-    else: print("(-1, -1)")
